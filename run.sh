e8015e551b1d21252295b44e73d9dc72ad24d56b
beginor <beginor@qq.com>
2016-05-21 10:33:20 +0800

    add restart parameter.

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-# --restart unless-stopped \
+
 docker run \
     --detach \
     --publish 8443:443 \
     --publish 8080:80 \
     --name gitlab \
+    --restart unless-stopped \
     --volume /mnt/sda1/gitlab/etc:/etc/gitlab \
     --volume /mnt/sda1/gitlab/log:/var/log/gitlab \
     --volume /mnt/sda1/gitlab/data:/var/opt/gitlab \
